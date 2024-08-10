9fdcfcc8f959a10593b02cb40c528fabb8ab3515
osoken <osoken@osoekawalab.com>
2024-08-11 00:57:54 +0900

    feat: add MemoryStorage

--- src/olstorage/storages/factory.py
+++ src/olstorage/storages/factory.py
@@ -1,6 +1,9 @@
-from ..settings import StorageSettings
+from ..settings import MemoryStorageSettings, StorageSettings
 from .base import BaseStorage
+from .memory import MemoryStorage
 
 
 def create_storage(settings: StorageSettings) -> BaseStorage:
+    if isinstance(settings, MemoryStorageSettings):
+        return MemoryStorage()
     raise ValueError("Invalid settings")
---
+++ src/olstorage/storages/memory.py
@@ -0,0 +1,5 @@
+from .base import BaseStorage
+
+
+class MemoryStorage(BaseStorage):
+    pass

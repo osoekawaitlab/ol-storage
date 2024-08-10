6c3006f1761b0106a56c2d0bda4706c9471e6d2e
osoken <osoken@osoekawalab.com>
2024-08-10 23:23:38 +0900

    fix: fix exported core name

--- tests/test_integration_module.py
+++ tests/test_integration_module.py
@@ -3,7 +3,7 @@ import olstorage
 
 def test_core_module() -> None:
     settings = olstorage.StorageCoreSettings(storage_settings={"type": "MEMORY"})
-    core = olstorage.core.create(settings=settings)
+    core = olstorage.StorageCore.create(settings=settings)
     document = core.Document(content="Hello, World!")
     saved_document = core.save(document)
     assert saved_document.content == "Hello, World!"
--- tests/test_package.py
+++ tests/test_package.py
@@ -5,3 +5,7 @@ import olstorage
 
 def test_olstorage_has_version() -> None:
     assert re.match(r"\d+\.\d+\.\d+", olstorage.__version__)
+
+
+def test_olstorage_core() -> None:
+    assert olstorage.StorageCore == olstorage.core.StorageCore

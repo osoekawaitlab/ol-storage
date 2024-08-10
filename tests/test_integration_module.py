0ea03abff5638b183491116dd4ca002e84613f7f
osoken <osoken@osoekawalab.com>
2024-08-11 00:36:51 +0900

    test: fix wrong import

--- tests/test_integration_module.py
+++ tests/test_integration_module.py
@@ -4,7 +4,7 @@ import olstorage
 def test_core_module() -> None:
     settings = olstorage.StorageCoreSettings(storage_settings={"type": "MEMORY"})
     core = olstorage.StorageCore.create(settings=settings)
-    document = core.Document(content="Hello, World!")
+    document = olstorage.Document(content="Hello, World!")
     saved_document = core.save(document)
     assert saved_document.content == "Hello, World!"
     actual = core.get(saved_document.id)

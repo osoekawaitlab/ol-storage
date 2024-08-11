8b866f475b03cc3896d2431a51593063cc0e97bf
osoken <osoken@osoekawalab.com>
2024-08-11 17:02:41 +0900

    test: add test for KeyError case

--- tests/test_integration_module.py
+++ tests/test_integration_module.py
@@ -12,3 +12,12 @@ def test_core_module() -> None:
     assert saved_document.content == "Hello, World!"
     actual = core.get(saved_document.id)
     assert actual.content == "Hello, World!"
+
+
+@pytest.mark.e2e
+def test_core_module_raises_key_error_when_document_not_found() -> None:
+    settings = olstorage.StorageCoreSettings(storage_settings={"type": "MEMORY"})
+    core = olstorage.StorageCore.create(settings=settings)
+    document = olstorage.Document(content="Hello, World!")
+    with pytest.raises(KeyError):
+        core.get(document.id)

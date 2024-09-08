12ea4fc295dcee6ca83cfc658d3201216bdc0d3e
osoken <osoken@osoekawalab.com>
2024-09-08 17:05:09 +0900

    fix: add function stub for KvsNexusLayer

--- src/olstorage/nexus_layers/kvs.py
+++ src/olstorage/nexus_layers/kvs.py
@@ -1,5 +1,17 @@
+from typing import Any
+
 from .base import BaseNexusLayer
 
 
 class KvsNexusLayer(BaseNexusLayer):
-    pass
+    def set(self, key: Any, value: Any) -> None:
+        raise NotImplementedError
+
+    def get(self, key: Any) -> Any:
+        raise NotImplementedError
+
+    def __len__(self) -> int:
+        raise NotImplementedError
+
+    def __contains__(self, key: Any) -> bool:
+        raise NotImplementedError
